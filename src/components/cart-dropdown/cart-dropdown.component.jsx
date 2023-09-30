56b779d6464f3255fc8b163c78b4e33ab9dc12b5
Gabriela411 <gabriela.toncu@yahoo.com>
2023-09-30 18:50:17 +0100

    Add products to the cart LOGIC

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,16 @@
 import './cart-dropdown.styles.scss';
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component'
+import { useCurrentCartDropdownState } from '../../contexts/cart.context';
 
-const CartDropdown = ({  }) => {
+const CartDropdown = () => {
+    const { cartItems } = useCurrentCartDropdownState();
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-
+                {
+                    cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)
+                }
             </div>
             <Button>Go to checkout</Button>
         </div>
