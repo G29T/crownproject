9705b4d6b9af6c18a9d667d2af2534c5a0c99692
Gabriela411 <gabriela.toncu@yahoo.com>
2023-10-01 14:24:05 +0100

    Implement checkout-item component

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,30 +1,49 @@
 import './checkout.styles.scss';
-import { useCurrentCartDropdownState } from '../../contexts/cart.context'
-import { useContext } from 'react';
+import { useCurrentCartDropdownState } from '../../contexts/cart.context';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 const Checkout = () => {
     const { cartItems, addItemToCart, removeItemFromCart } = useCurrentCartDropdownState();
 
     return(
-        <div className='cart-item-container'>
-            <div>
-                {
-                    cartItems.map((cartItem) => {
-                        const { id, name, imageUrl, price, quantity } = cartItem;
-                        return ( 
-                            <div key={id}>
-                                <img src={imageUrl} alt={`${name}`}/>
-                                <span className='name'>{ name }</span>
-                                <span className='price'>{quantity} x ${price}</span>
-                                <br />
-                                <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
-                                <br />
-                                <span onClick={() => addItemToCart(cartItem)}>increment</span>
-                            </div>
-                        )
-                    })
-                }
+        <div className='checkout-container'>
+            <div  className='checkout-header'>
+                <div  className='header-block'>
+                    <span>Product</span>
+                </div>
+                <div  className='header-block'>
+                    <span>Description</span>
+                </div>
+                <div  className='header-block'>
+                    <span>Quantity</span>
+                </div>
+                <div  className='header-block'>
+                    <span>Price</span>
+                </div>
+                <div  className='header-block'>
+                    <span>Remove</span>
+                </div>
             </div>
+            {
+                cartItems.map((cartItem) => {
+                    return(
+                        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+                    )
+                    // const { id, name, imageUrl, price, quantity } = cartItem;
+                    // return ( 
+                    //     <div key={id}>
+                    //         <img src={imageUrl} alt={`${name}`}/>
+                    //         <span className='name'>{ name }</span>
+                    //         <span className='price'>{quantity} x ${price}</span>
+                    //         <br />
+                    //         <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
+                    //         <br />
+                    //         <span onClick={() => addItemToCart(cartItem)}>increment</span>
+                    //     </div>
+                    // )
+                })
+            }
+            <span className='Total'>Total</span>
         </div>
     )
 }
