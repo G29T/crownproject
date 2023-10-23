dbc3d1fdd382a170c65f72eb3c6511729c1ac838
Gabriela411 <gabriela.toncu@yahoo.com>
2023-10-23 08:36:04 +0100

    Link the directory-items to the appropriate page (useNavigate Hook)

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -5,11 +5,17 @@ import {
     Body
   } from './directory-item.styles';
 
+import { useNavigate } from 'react-router-dom'; //Hook
+
 // this is a functional component
 const DirectoryItem = ({category}) => {
-    const {id, imageUrl,title} = category
+    const {imageUrl, title, route} = category
+    const navigate = useNavigate();
+    
+    const onNavigateHandler = () => navigate(route);
+
     return (
-        <DirectoryItemContainer>
+        <DirectoryItemContainer onClick={onNavigateHandler}>
             <BackgroundImage imageUrl={imageUrl} />
             <Body>
                 <h2>{title}</h2>
