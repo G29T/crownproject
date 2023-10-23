086432a8b8fed46f6a7b30fdec787e333d55e6dd
Gabriela411 <gabriela.toncu@yahoo.com>
2023-10-23 08:25:10 +0100

    STYLED form-input component (the most complex one)
    - Contains mixins
    - The order of styled components inside the file is important!

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,14 +1,16 @@
-import './form-input.styles.scss';
+import { FormInputLabel, Input, Group } from './form-input.styles';
 
-const FormInput = ({ label, inputProps }) => {
-    return(
-        <div className="group">
-            <input className="form-input" {...inputProps} />
-            {label && (
-                <label className={`${inputProps.value.length ? 'shrink' : ''} form-input-label`}>{label}</label>
-            )}
-        </div>
-    );
+const FormInput = ({ label, ...otherProps }) => {
+  return (
+    <Group>
+      <Input {...otherProps} />
+      {label && (
+        <FormInputLabel shrink={otherProps.value.length}>
+          {label}
+        </FormInputLabel>
+      )}
+    </Group>
+  );
 };
 
 export default FormInput;
